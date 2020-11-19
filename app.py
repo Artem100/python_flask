368ff3b26926b382b9ed472a15a3058d3bbf93f7
Artem <some@some.coom>
2020-11-19 21:34:48 +0200

    working method encrypt

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+key = Fernet.generate_key()
+f = Fernet(key)
 
 @app.route('/')
 @app.route('/<name>')
@@ -10,20 +12,21 @@ def index(name='Name'):
     title = request.args.get('title', 'default')
     return render_template('index.html', name=name, title=title)
 
-@app.route('/encrypt?string=<string_to_encrypt>')
-@app.route('/<string_to_encrypt>')
-def string_to_encrypt_method(name='string_to_encrypt'):
-    f = Fernet(name)
-    return render_template('index.html', name=name)
+@app.route('/encrypt')
+def string_to_encrypt_method():
+    title = request.args.get('param', 'default')
+    token = f.encrypt(title)
+    return render_template('index.html', name=token, title=title)
 
-@app.route('/decrypt?string=<string_to_decrypt>')
-@app.route('/<name>')
-def string_to_encrypt_method(name='string_to_decrypt'):
-    title = request.args.get('title', 'default')
-    return render_template('index.html', name=name, title=title)
+
+@app.route('/decrypt')
+def string_to_decrypt_method():
+    title = request.args.get('param', 'default')
+    dec = f.decrypt(title)
+    return render_template('index.html', name=dec, title=title)
 
 @app.route('/catalog')
 def catalog():
     return "catalog!!!"
 
-app.run(debug=True, port=5005)
+app.run(debug=True, port=5009)
