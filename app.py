ae6ae31d325f5b17885343a1498a959de73b65fc
Artem <some@some.coom>
2020-11-19 11:44:27 +0200

    first commit flask

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def index(name='Name'):
+    title = request.args.get('title', 'default')
+    return render_template('index.html', name=name, title=title)
+
+@app.route('/catalog')
+def catalog():
+    return "catalog!!!"
+
+app.run(debug=True, port=5005)
