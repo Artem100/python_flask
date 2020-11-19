50733331570f3222cede9f4e78b056a0394cae4b
Artem <some@some.coom>
2020-11-19 21:17:51 +0200

    Created session and cookie counter

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, session
 
 app = Flask(__name__)
+app.secret_key = b'093rj3984fjrfj'
 
 @app.route('/')
-def index(name='Name'):
-    title = request.args.get('title', 'default')
-    return render_template('index.html', name=name, title=title)
+def index():
+    # Работа с куками
+    visited = 0
+    if request.cookies.get('visited'):
+        visited = int(request.cookies.get('visited'))
+    resp = make_response(render_template('index.html', visited=visited))
+    resp.set_cookie('visited', str(visited+1))
+    return resp
+
+@app.route('/session')
+def session_page():
+    var_session = 0
+    if session.get('var_session'):
+        var_session = int(session.get('var_session'))
+    else:
+        session['var_session'] = 0
+
+    resp = make_response(render_template('index.html', var_session=var_session))
+    session['var_session'] += 1
+    return resp
 
 @app.route('/catalog', methods=['GET', 'POST'])
 def catalog_post():
@@ -19,4 +37,4 @@ def catalog_post():
 # def catalog():
 #     return "catalog!!!"
 
-app.run(debug=True, port=5005)
+app.run(debug=True, port=5007)
