ef3ad52e7623cc5d9824e83dfe3d04313dd3ec37
Artem <some@some.coom>
2020-11-19 19:43:07 +0200

    Method post in flask

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ def index(name='Name'):
     title = request.args.get('title', 'default')
     return render_template('index.html', name=name, title=title)
 
-@app.route('/catalog')
-def catalog():
-    return "catalog!!!"
+@app.route('/catalog', methods=['POST'])
+def catalog_post():
+    param = request.form['param']
+    return param
+#
+# @app.route('/catalog')
+# def catalog():
+#     return "catalog!!!"
 
 app.run(debug=True, port=5005)
